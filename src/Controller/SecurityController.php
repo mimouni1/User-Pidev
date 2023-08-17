332c150dd24557eb4f78aa06e99093c988cab599
[mimouni1] <80392051+mimouni1@users.noreply.github.com>
2023-08-17 20:15:08 +0100

    template twig base

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -29,4 +29,11 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    #[Route(path: '/a', name: 'index')]
+    public function indexlogin() : Response
+    {
+    
+        return $this->render('base.html.twig');
+    }
 }
